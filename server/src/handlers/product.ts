c375e3fed52159d89edadd6a19746fc375261c27
Daniel Molina <danielmolina.dev@gmail.com>
2025-04-05 19:12:50 +0200

    feat: add get all products endpoint

--- server/src/handlers/product.ts
+++ server/src/handlers/product.ts
@@ -1,6 +1,19 @@
 import { Request, Response } from "express";
 import Product from "../models/Product.model";
 
+export const getProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await Product.findAll({
+      order: [["id", "ASC"]],
+      attributes: { exclude: ["createdAt", "updatedAt"] },
+    });
+    res.status(200).json({ data: products });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error getting products" });
+  }
+};
+
 export const createProduct = async (req: Request, res: Response) => {
   try {
     const product = await Product.create(req.body);
