f86b8d892d85ab35c28498a6518a3d65b217996a
Daniel Molina <danielmolina.dev@gmail.com>
2025-04-05 16:24:15 +0200

    feat: add db connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,23 @@
 import express from "express";
+import colors from "colors";
 import router from "./router";
+import db from "./config/db";
+
+// Connect to the database
+async function connectDB() {
+  try {
+    await db.authenticate();
+    db.sync();
+    console.log(colors.blue.bold("Database connected"));
+  } catch (error) {
+    console.log(
+      colors.white.bgRed.bold("Error connecting to the database"),
+      error
+    );
+  }
+}
+
+connectDB();
 
 const server = express();
 
