893510c152dd04d8df641d3d06ac6096eb1e2c83
Daniel Molina <danielmolina.dev@gmail.com>
2025-04-05 18:36:24 +0200

    feat: add middleware to handle input validations

--- server/src/handlers/product.ts
+++ server/src/handlers/product.ts
@@ -1,31 +1,12 @@
 import { Request, Response } from "express";
-import { check, validationResult } from "express-validator";
 import Product from "../models/Product.model";
 
 export const createProduct = async (req: Request, res: Response) => {
-  // Validations
-  await check("name")
-    .notEmpty()
-    .withMessage("Name is required")
-    .isLength({ min: 3 })
-    .withMessage("Name must be at least 3 characters long")
-    .run(req);
-  await check("price")
-    .notEmpty()
-    .withMessage("Price is required")
-    .isNumeric()
-    .withMessage("Price must be a number")
-    .isFloat({ min: 0 })
-    .withMessage("Price must be greater than 0")
-    .run(req);
-
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    res.status(400).json({ errors: errors.array() });
-    return;
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json({ data: product });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error creating product" });
   }
-
-  const product = await Product.create(req.body);
-
-  res.status(201).json({ data: product });
 };
--- server/src/router.ts
+++ server/src/router.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { createProduct } from "./handlers/product";
+import { handleInputErrors } from "./middleware";
 
 const router = Router();
 
@@ -7,6 +9,24 @@ router.get("/", (req, res) => {
   res.send("From GET");
 });
 
-router.post("/", createProduct);
+router.post(
+  "/",
+  [
+    body("name")
+      .notEmpty()
+      .withMessage("Name is required")
+      .isLength({ min: 3 })
+      .withMessage("Name must be at least 3 characters long"),
+    body("price")
+      .notEmpty()
+      .withMessage("Price is required")
+      .isNumeric()
+      .withMessage("Price must be a number")
+      .isFloat({ min: 0 })
+      .withMessage("Price must be greater than 0"),
+  ],
+  handleInputErrors,
+  createProduct
+);
 
 export default router;
