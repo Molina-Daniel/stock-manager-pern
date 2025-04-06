970df6f6d4c85fedfca4275d384ae18acbefcc28
Daniel Molina <danielmolina.dev@gmail.com>
2025-04-06 20:29:09 +0200

    refactor: improve test coverage removing unused code

--- server/src/handlers/product.ts
+++ server/src/handlers/product.ts
@@ -2,86 +2,56 @@ import { Request, Response } from "express";
 import Product from "../models/Product.model";
 
 export const getProducts = async (req: Request, res: Response) => {
-  try {
-    const products = await Product.findAll({
-      order: [["id", "ASC"]],
-      attributes: { exclude: ["createdAt", "updatedAt"] },
-    });
-    res.status(200).json({ data: products });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Error getting products" });
-  }
+  const products = await Product.findAll({
+    order: [["id", "ASC"]],
+    attributes: { exclude: ["createdAt", "updatedAt"] },
+  });
+  res.status(200).json({ data: products });
 };
 
 export const getProductById = async (req: Request, res: Response) => {
-  try {
-    const product = await Product.findByPk(req.params.id, {
-      attributes: { exclude: ["createdAt", "updatedAt"] },
-    });
-    if (!product) {
-      res.status(404).json({ error: "Product not found" });
-      return;
-    }
-    res.status(200).json({ data: product });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Error getting product" });
+  const product = await Product.findByPk(req.params.id, {
+    attributes: { exclude: ["createdAt", "updatedAt"] },
+  });
+  if (!product) {
+    res.status(404).json({ error: "Product not found" });
+    return;
   }
+  res.status(200).json({ data: product });
 };
 
 export const createProduct = async (req: Request, res: Response) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(201).json({ data: product });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Error creating product" });
-  }
+  const product = await Product.create(req.body);
+  res.status(201).json({ data: product });
 };
 
 export const updateProduct = async (req: Request, res: Response) => {
-  try {
-    const product = await Product.findByPk(req.params.id);
-    if (!product) {
-      res.status(404).json({ error: "Product not found" });
-      return;
-    }
-    await product.update(req.body); // This update method protects any field not sent on a PUT request
-    res.status(200).json({ data: product });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Error updating product" });
+  const product = await Product.findByPk(req.params.id);
+  if (!product) {
+    res.status(404).json({ error: "Product not found" });
+    return;
   }
+  await product.update(req.body); // This update method protects any field not sent on a PUT request
+  res.status(200).json({ data: product });
 };
 
 export const updateAvailability = async (req: Request, res: Response) => {
-  try {
-    const product = await Product.findByPk(req.params.id);
-    if (!product) {
-      res.status(404).json({ error: "Product not found" });
-      return;
-    }
-    product.availability = !product.dataValues.availability;
-    await product.save();
-    res.status(200).json({ data: product });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Error updating product" });
+  const product = await Product.findByPk(req.params.id);
+  if (!product) {
+    res.status(404).json({ error: "Product not found" });
+    return;
   }
+  product.availability = !product.dataValues.availability;
+  await product.save();
+  res.status(200).json({ data: product });
 };
 
 export const deleteProduct = async (req: Request, res: Response) => {
-  try {
-    const product = await Product.findByPk(req.params.id);
-    if (!product) {
-      res.status(404).json({ error: "Product not found" });
-      return;
-    }
-    await product.destroy();
-    res.status(204).json({ data: "Product deleted" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Error deleting product" });
+  const product = await Product.findByPk(req.params.id);
+  if (!product) {
+    res.status(404).json({ error: "Product not found" });
+    return;
   }
+  await product.destroy();
+  res.status(204).json({ data: "Product deleted" });
 };
